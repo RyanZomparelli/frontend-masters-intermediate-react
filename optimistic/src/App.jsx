f97202c9cff16dc981149c4a9b3f59e7a9fe7eb8
Ryan Zomparelli <zomp.ryan@gmail.com>
2026-05-04 14:15:21 -0400

    optimistic: code App.jsx to set up useOptimistic example

---
+++ optimistic/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [thoughts, setThoughts] = useState([]);
+  const [thought, setThought] = useState("");
+
+  async function postDeepThought() {
+    setThought("");
+
+    const res = await fetch("/thoughts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ thought }),
+    });
+
+    if (!res.ok) {
+      alert("This thought was not deep enough. Please try again.");
+      return;
+    }
+
+    const { thoughts: newThoughts } = await res.json();
+    setThoughts(newThoughts);
+  }
+
+  useEffect(() => {
+    fetch("/thoughts")
+      .then((res) => res.json())
+      .then((data) => setThoughts(data));
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>Deep Thoughts</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          postDeepThought();
+        }}
+      >
+        <label htmlFor="thought">What's on your mind?</label>
+        <textarea
+          id="thought"
+          name="thought"
+          rows="5"
+          cols="33"
+          value={thought}
+          onChange={(e) => setThought(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {thoughts.map((thought) => (
+          <li key={thought}>{thought}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
